fb754ee7e42a8082b8215ac8fa77b6387009e506
mtmanr0e <mtmanr0e@gmail.com>
2025-08-06 19:11:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const cors = require('cors');
-app.use(cors());
 const dotenv = require("dotenv");
 
 dotenv.config();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
